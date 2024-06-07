3f2e065e96e3e0bed4255a957c232f19893b6c22
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-07 13:29:16 +0300

    bug

--- go/slotmachine.go
+++ go/slotmachine.go
@@ -8,19 +8,19 @@ func main(){
 	var running bool = true
 
 	for running{
-		
+		var value string
 		// fmt.Println("Your current game balnce is:", balance," shillings")
 		fmt.Println("Press {ENTER} to continue  or {Q} to quit the game")
-		value,err := fmt.Scanln()
-		value = string(value)
-		
+		_,err := fmt.Scan(&value)
+
 		if err != nil{
-			fmt.Println("Error found; ")
+			fmt.Println("Error found; ",err)
 			continue
 		}
 		
-		if value[0] == "\n"{
+		if string(value[0]) == "\n"{
 			fmt.Println("Continuing game")
 		}
+		fmt.Println("now")
 	}
 }
